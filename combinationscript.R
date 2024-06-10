d605871b2657351f2c5615fa9a5017fc8d38319f
laurenappleby <laurenappleby07@gmail.com>
2024-06-10 12:44:48 +0100

    Harmonizing measurement colummn, cleaning up other things.

--- combinationscript.R
+++ combinationscript.R
@@ -4,12 +4,12 @@
 library(dplyr)
 
 # First subsetting the PREDICTS database to just keep the relevant columns
-PREDICTS_subset <- PREDICTS_mammalia %>% select(Source_ID , Reference , Diversity_metric_type , 
+PREDICTS_subset <- PREDICTS_mammalia %>% select(Reference , Diversity_metric_type , 
                                                 Diversity_metric_is_effort_sensitive , SSS , SSB, 
                                                 Sampling_effort , Habitat_as_described , Longitude , 
                                                 Latitude , Country , Ecoregion , Biome , Order , Family , 
                                                 Genus , Species , Best_guess_binomial , Measurement, 
-                                                Sample_start_earliest , Total_abundance, Species_richness)
+                                                Sample_start_earliest)
 
 # Merging the mammal communities data
 MCDB_sites_trapping <- merge(MCDB_sites, MCDB_trapping, by = "Site_ID", all = FALSE)
@@ -29,16 +29,14 @@ MCDB_subset <- MCDB_subset %>% select(Site_ID , Initial_year , Presence_only , A
                                       Species , Country , Latitude , Longitude , Habitat_description , Trap_nights ,
                                       Reference_ID , Species_richness)
 
-#Renaming variables so they bind in the same column
-names(MCDB_subset)[names(MCDB_subset) == "Reference_ID"] <- "Reference"
-names(PREDICTS_subset)[names(PREDICTS_subset) == "Habitat_as_described"] <- "Habitat_description"
-names(MCDB_subset)[names(MCDB_subset) == "Trap_nights"] <- "Sampling_effort"
-names(PREDICTS_subset)[names(PREDICTS_subset) == "Sample_start_earliest"] <- "Initial_year"
-names(MCDB_subset)[names(MCDB_subset) == "Site_ID"] <- "Site_ID/SSS"
-names(PREDICTS_subset)[names(PREDICTS_subset) == "SSS"] <- "Site_ID/SSS"
-names(PREDICTS_subset)[names(PREDICTS_subset) == "Total_abundance"] <- "Site_abundance"
+# Changing the Presence Only column from binary 0 and 1 to character Abundance/Occurrence
+MCDB_subset$Presence_only[MCDB_subset$Presence_only == 0] <- "Abundance"
+MCDB_subset$Presence_only[MCDB_subset$Presence_only == 1] <- "Occurrence"
 
-# Putting them together
+# Changing the NULL to 1 (For occurrence data binary 0/1 for present or absent)
+MCDB_subset$Presence_only[MCDB_subset$Presence_only == "NULL"] <- 1
+
+# Changing types of data so the columns can match up 
 PREDICTS_subset$Sampling_effort <- as.numeric(PREDICTS_subset$Sampling_effort)
 MCDB_subset$Sampling_effort <- as.numeric(MCDB_subset$Sampling_effort)
 as.numeric(PREDICTS_subset$Sampling_effort)
@@ -58,9 +56,27 @@ as.character(PREDICTS_subset$Initial_year)
 MCDB_subset$Abundance <- as.double(MCDB_subset$Abundance)
 as.double(MCDB_subset$Abundance)
 
+#Renaming columns so they match up
+names(MCDB_subset)[names(MCDB_subset) == "Reference_ID"] <- "Reference"
+names(PREDICTS_subset)[names(PREDICTS_subset) == "Habitat_as_described"] <- "Habitat_description"
+names(MCDB_subset)[names(MCDB_subset) == "Trap_nights"] <- "Sampling_effort"
+names(PREDICTS_subset)[names(PREDICTS_subset) == "Sample_start_earliest"] <- "Initial_year"
+names(MCDB_subset)[names(MCDB_subset) == "Site_ID"] <- "Site_ID/SSS"
+names(PREDICTS_subset)[names(PREDICTS_subset) == "SSS"] <- "Site_ID/SSS"
+names(PREDICTS_subset)[names(PREDICTS_subset) == "Total_abundance"] <- "Site_abundance"
+names(MCDB_subset)[names(MCDB_subset) == "Presence_only"] <- "Diversity_metric_type"
+names(MCDB_subset)[names(MCDB_subset) == "Abundance"] <- "Measurement"
+
+#Putting them together
 dd <- bind_rows(PREDICTS_subset, MCDB_subset)
 
+# Adding Diversity Metric TRUE/FALSE to the MCDB data within the combined frame
+dd$Diversity_metric_is_effort_sensitive[dd$Diversity_metric_type == "Occurrence"] <- FALSE
+dd$Diversity_metric_is_effort_sensitive[dd$Diversity_metric_type == "Abundance"] <- TRUE
+
 # Rescaling Sampling Effort adding column for max effort
+dd$Sampling_effort[dd$Diversity_metric_type == "Occurrence"] <- 1
+
 dd <- dd %>%
   group_by(Reference) %>%
   mutate(max_effort = max(Sampling_effort))
@@ -72,7 +88,13 @@ dd <- dd %>%
 #adding column for rescaling factor
 dd$rescaling_factor = 1/dd$effort_rescaled
 
+#adding a column for plain abundance 
+dd <- dd %>% 
+  mutate(Abundance = Measurement)
+dd$Abundance[dd$Diversity_metric_type == "Occurrence"] <- NA
 
+#adding a column for rescaled abundance
+dd$Abundance_rescaled = dd$rescaling_factor * dd$Measurement
 
 # Removing sites that don't have long/lat information and sites with invalid longitude information (17)
 dd <- dd[complete.cases(dd$Longitude, dd$Latitude), ]
